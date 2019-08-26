9de50325fbc4c98844374151af36acbac08cd292
FW <felix.wieland@mail.de>
2019-08-26 18:34:46 +0200

    FuzzyAutosuggestion class

---
+++ src/server/FuzzyAutosuggest.js
@@ -0,0 +1,65 @@
+import Fuse from "fuse.js"
+import mongodb from "mongodb"
+
+class FuzzyAutosuggest {
+
+    constructor(connectionString) {
+        this.options = {}
+        this.dataset = []
+        this.fuse = undefined
+        this.loading = false
+        this.connectionString = connectionString
+        this.setOptions()
+    }
+
+    loadDataset(ready) {
+        var MongoClient = mongodb.MongoClient
+        this.loading = true
+        MongoClient.connect(this.connectionString, { useNewUrlParser: true }, function (err, conn) {
+            if (err) {
+                throw err
+            }
+            var db = conn.db("INEZ")
+            db.collection("products").find({}).toArray(function (err, result) {
+                if (err) throw err
+                ready(result)
+            })
+        })
+    }
+
+    setOptions() {
+        this.options = {
+            shouldSort: true,
+            threshold: 0.4,
+            location: 0,
+            distance: 6,
+            maxPatternLength: 20,
+            keys: ['name']
+        };
+    }
+
+    initializeFuse(dataset) {
+        this.fuse = new Fuse(dataset, this.options)
+        return this.fuse
+    }
+
+    runSearch(toSeachFor, respond, limit) {
+        if (this.loading === true) throw "Currently loading"
+        var that = this;
+        if (this.fuse === undefined) {
+            this.loadDataset(function (result) {
+                var fuse = that.initializeFuse(result)
+                that.loading = false
+                if (limit === undefined)
+                    respond(fuse.search(toSeachFor))
+                else
+                    respond(fuse.search(toSeachFor).slice(0, limit))
+
+            })
+            return
+        }
+        respond(this.fuse.search(toSeachFor))
+    }
+}
+
+export default FuzzyAutosuggest
