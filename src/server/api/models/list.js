8410dea9eddd8f8f54c673450f1ef583997db4db
FW <felix.wieland@mail.de>
2019-08-31 00:54:37 +0200

    Fixed Merge Conflict

---
+++ src/server/api/models/list.js
@@ -0,0 +1,16 @@
+const mongoose = require("./mongooseConnection").mongoose;
+
+const shoppingListSchema = mongoose.Schema({
+	list_name: { type: String, required: true },
+	_id: mongoose.Schema.Types.ObjectId,
+	products: [
+		{
+			_id: { type: Schema.Types.ObjectId, ref: "products", required: true },
+			measure: String,
+			amount: { type: Number, min: 0 },
+			grou: { type: String, required: true }
+		}
+	]
+});
+
+module.exports = mongoose.model("shopping_List", shoppingListSchema);
